216324cab7f80e57d5da857c4215d78c1897fc1c
Angelah Mbula Maweu <104361772+AngelahMbula@users.noreply.github.com>
2023-03-21 12:53:08 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 from math import factorial
 def pascal_triangle(n):
-    '''
-    returns list of integers representing pascals triangle
-    '''
     if n <= 0:
         return []
     for i in range(n):
