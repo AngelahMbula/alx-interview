c26d0f47beb400b4ba93fd8d7d963b095b2ef9fb
Angelah Mbula <mbula.angelah@gmail.com>
2023-03-21 02:42:22 -0700

    initial commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,11 +4,7 @@ def pascal_triangle(n):
         return []
     for i in range(n):
         for j in range(n-i+1):
-            
-            # for left spacing
             print(end=" ")
-            
         for j in range(i+1):
             print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
-            
         print()
