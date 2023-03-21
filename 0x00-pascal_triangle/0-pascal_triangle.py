f33e774e5facd4cdda15ba9eac79274ef847c0ac
AngelahMbula <mbula.angelah@gmail.com>
2023-03-21 09:21:38 +0000

    initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Function returns list of int rep Pascal triangle"""
+from math import factorial
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of intergers
+    """
+    if n <= 0:
+        return []
+    for i in range(n):
+        for j in range(n-i+1):
+            
+            # for left spacing
+            print(end=" ")
+            
+        for j in range(i+1):
+            print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+            
+        print()
