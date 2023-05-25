57d11fd6c5d4c82530960e8e5b7bc1879c78b08f
AngelahMbula <mbula.angelah@gmail.com>
2023-05-25 10:50:14 +0000

    initial commit

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,19 +2,19 @@
 def island_perimeter(grid):
     rows = len(grid)
     cols = len(grid[0])
-    
+
     perimeter = 0
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Each land cell contributes 4 to the perimeter
-                
+                perimeter += 4  # Each land cell contributes 4 to the perimete
+
                 # Check neighboring cells
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 if the neighbor is also land horizontally
-                    
+                    perimeter -= 2
+
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 if the neighbor is also land vertically
-    
+                    perimeter -= 2
+
     return perimeter
